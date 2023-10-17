0c64183765cf4c6eb5d57ce8e064c7ffe22dc790
Levent Bal <e227405@edu.sbl.ch>
2023-10-17 11:23:57 +0200

    gelösst 1-3, 4 angefangen

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ function sayHello() {
 
 function changeBackground() {
   const body = document.querySelector("body")
-  body.style.backgroundColor = "darkgrey"
+  body.style.backgroundColor = "red"
+}
+
+function deleteText (){
+  const elemtodeletetext = document.querySelector("#to-delete")
+  elemtodeletetext.textContent = "Inhalt gelöscht"
+  
+
+}
+function counter() {
+  const numberElement = document.querySelector("#zahl")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
 }
